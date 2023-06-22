2cf701b76ee071a58e44e251c37ea1086b8d013b
canna88 <97223496+canna88@users.noreply.github.com>
2023-06-22 07:40:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Domanda 1
+    // Number: è un tipo di variabile che contiene un numero, che può essere float o int
+    // String: contiene una serie di caratterti che, nella fase di definizione, devono essere contenuti nelle virgolette ""
+    // Boolean: è una variabile di tipo logico, può contenere 2 valori: True o False
+    // Undefined: è una variabile il cui contenuto non è ancora stato definito
+    // Null: è una variabile che contiene il valore null, un valore vuoto
+
+let rispostaDomanda1 = "un algoritmo si può definire come un insieme di azioni/step ordinati da effettuare per ottenere un certo risultato"
+console.log("Domanda 1: ",rispostaDomanda1)
+
+//Domanda 2
+
+let rispostaDomanda2 = "una variabile può essere definita come un contenitore al cui interno vi è un elemento di un certo tipo"
+console.log("Domanda 2: ",rispostaDomanda2)
+
+//Domanda 3
+
+let rispostaDomanda3 = "Una variabile Undefined ha un contenuto che non è ancora stato definito mentre una variabile Null contiene, appunto, il valore null, un valore vuoto, ma specificato"
+console.log("Domanda 3: ",rispostaDomanda3)
+
